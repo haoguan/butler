9ffa9d7d3e786a01cf72759474daa3d71f730a80
haoguan <haohowardguan@gmail.com>
2017-01-30 23:52:31 -0800

    updated user controller test comments

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -17,7 +17,6 @@ defmodule Butler.UserControllerTest do
 
   test "GET api/v1/users/id", context do
     %{user1: mock_user} = context[:users]
-    # Assert created user and queried user has the same ID
     conn = get build_conn(), Enum.join(["api/v1/users/", mock_user.id]), nil
     %{"description" => description, "status" => status,
       "data" => %{"id" => get_id, "alexa_id_hash" => _}} = json_response(conn, 200)
@@ -28,12 +27,12 @@ defmodule Butler.UserControllerTest do
 
   test "GET api/v1/users", context do
     %{user1: mock_user, user2: mock_user_2} = context[:users]
-    # Assert created user and queried user has the same ID
     conn = get build_conn(), "api/v1/users", nil
     %{"description" => description, "status" => status,
       "data" => [%{"id" => index_id_1}, %{"id" => index_id_2}]} = json_response(conn, 200)
     assert status == 200
     assert description == "Operation successfully completed"
+    # Assert response data user ids contains expected mock user ids
     assert Enum.member?([index_id_1, index_id_2], mock_user.id)
     assert Enum.member?([index_id_1, index_id_2], mock_user_2.id)
   end
