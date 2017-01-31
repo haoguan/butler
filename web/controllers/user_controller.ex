6d3a71002b97b2a4e9a2ab93024b4d454eaf9045
haoguan <haohowardguan@gmail.com>
2017-01-30 23:29:46 -0800

    added user controller

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,29 @@
+defmodule Butler.API.V1.UserController do
+  use Butler.Web, :controller
+  alias Butler.User
+
+  def index(conn, _params) do
+    users = Repo.all(User)
+    ResponseController.render_data(conn, users)
+  end
+
+  def show(conn, %{"id" => id}) do
+    case user = Repo.get(User, id) do
+      nil ->
+        ResponseController.not_found(conn)
+      _ ->
+        ResponseController.render_data(conn, user)
+    end
+end
+
+  def create(conn, user_params) do
+    changeset = User.registration_changeset(user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        ResponseController.render_created(conn, user, "User successfully created")
+      {:error, changeset} ->
+        ResponseController.changeset_error(conn, changeset)
+    end
+  end
+
+end
