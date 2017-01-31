a5ecf692c26611c976db31dce978ef9153ac06ec
haoguan <haohowardguan@gmail.com>
2017-01-30 23:50:49 -0800

    added user controller index test

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -2,12 +2,13 @@ defmodule Butler.UserControllerTest do
   use Butler.ConnCase
 
   setup do
-    user = insert_mock_user()
-    {:ok, user: user}
+    user = insert_mock_user("amzn1.test.user1.id")
+    user2 = insert_mock_user("amzn1.test.user2.id")
+    {:ok, users: %{user1: user, user2: user2}}
   end
 
   test "POST api/v1/users" do
-    conn = post build_conn(), "api/v1/users", [alexa_id: "amzn1.test.id"]
+    conn = post build_conn(), "api/v1/users", [alexa_id: "amzn1.test.postuser.id"]
     %{"description" => description, "status" => status,
       "data" => %{"id" => _, "alexa_id_hash" => _}} = json_response(conn, 201)
     assert status == 201
@@ -15,7 +16,7 @@ defmodule Butler.UserControllerTest do
   end
 
   test "GET api/v1/users/id", context do
-    mock_user = context[:user]
+    %{user1: mock_user} = context[:users]
     # Assert created user and queried user has the same ID
     conn = get build_conn(), Enum.join(["api/v1/users/", mock_user.id]), nil
     %{"description" => description, "status" => status,
@@ -23,5 +24,17 @@ defmodule Butler.UserControllerTest do
     assert status == 200
     assert description == "Operation successfully completed"
     assert mock_user.id == get_id
+  end
+
+  test "GET api/v1/users", context do
+    %{user1: mock_user, user2: mock_user_2} = context[:users]
+    # Assert created user and queried user has the same ID
+    conn = get build_conn(), "api/v1/users", nil
+    %{"description" => description, "status" => status,
+      "data" => [%{"id" => index_id_1}, %{"id" => index_id_2}]} = json_response(conn, 200)
+    assert status == 200
+    assert description == "Operation successfully completed"
+    assert Enum.member?([index_id_1, index_id_2], mock_user.id)
+    assert Enum.member?([index_id_1, index_id_2], mock_user_2.id)
   end
 end
