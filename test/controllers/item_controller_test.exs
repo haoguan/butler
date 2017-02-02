72b8c7a641916f1fb4bacb0492fde04416c6af2b
haoguan <haohowardguan@gmail.com>
2017-02-01 23:13:56 -0800

    test for trailing words

--- test/controllers/item_controller_test.exs
+++ test/controllers/item_controller_test.exs
@@ -9,7 +9,6 @@ defmodule Butler.ItemControllerTest do
 
   test "POST api/v1/users/user_id/items", context do
     %{user1: mock_user} = context[:users]
-    # {:ok, start_date} = Timex.now()
     conn = post build_conn(), Enum.join(["api/v1/users/", mock_user.id]) <> "/items", [raw_term: "kitchen towels"]
     %{"description" => description, "status" => status,
       "data" => %{"id" => _, "type" => type, "modifier" => modifier,
@@ -23,6 +22,23 @@ defmodule Butler.ItemControllerTest do
 
     # negative if first datetime occurs before second
     assert Timex.diff(Timex.now(), expiration, :months) == -1
+  end
+
+  test "POST api/v1/users/user_id/items with trailing words", context do
+    %{user1: mock_user} = context[:users]
+    conn = post build_conn(), Enum.join(["api/v1/users/", mock_user.id]) <> "/items", [raw_term: "Angelina's clarisonic for shower"]
+    %{"description" => description, "status" => status,
+      "data" => %{"id" => _, "type" => type, "modifier" => modifier,
+      "expiration_date" => expiration_date}} = json_response(conn, 201)
+
+    {:ok, expiration} = convert_ISO_to_Timex(expiration_date)
+    assert status == 201
+    assert description == "Item successfully created"
+    assert type == "clarisonic"
+    assert modifier == "Angelina's"
+
+    # negative if first datetime occurs before second
+    assert Timex.diff(Timex.now(), expiration, :months) == -3
   end
 
 end
