02856184122e04a1643f91b335f8262839c6da4c
haoguan <haohowardguan@gmail.com>
2017-04-06 01:18:41 -0700

    refactored relative response into method and added tests

---
+++ test/modules/date_interpreter_test.exs
@@ -0,0 +1,45 @@
+defmodule Butler.DateInterpreterTest do
+  use Butler.ConnCase
+  use ExUnit.Case
+
+  alias Butler.DateInterpreter
+
+  test "future expiration date using days" do
+    test_start_date = Timex.to_datetime({2017, 4, 4})
+    {:ok, _, relative_expiration} = DateInterpreter.interpret_expiration("in twenty days", test_start_date)
+    assert relative_expiration == "in 20 days, on Monday, April 24, 2017"
+  end
+
+  test "future expiration using months and days" do
+    test_start_date = Timex.to_datetime({2017, 4, 4})
+    {:ok, _, relative_expiration} = DateInterpreter.interpret_expiration("in three months and twelve days", test_start_date)
+    # NOTE: Relative component will only specify the largest unit
+    assert relative_expiration == "in 3 months, on Sunday, July 16, 2017"
+  end
+
+  test "future expiration using irregular months and days" do
+    test_start_date = Timex.to_datetime({2017, 4, 4})
+    {:ok, _, relative_expiration} = DateInterpreter.interpret_expiration("in three months and zero days", test_start_date)
+    assert relative_expiration == "in 3 months, on Tuesday, July 4, 2017"
+  end
+
+  test "future expiration shifted by hours" do
+    test_start_date = Timex.to_datetime({{2017, 4, 4}, {10, 25, 0}})
+    {:ok, _, relative_expiration} = DateInterpreter.interpret_expiration("in eight hours", test_start_date)
+    # TODO: Add quality of life change to exchange full date with "today" if makes sense.
+    assert relative_expiration == "in 8 hours, on Tuesday, April 4, 2017 at 6:25PM"
+  end
+
+  test "future expiration shifted by hours that spills into the next day" do
+    test_start_date = Timex.to_datetime({{2017, 4, 4}, {10, 25, 0}})
+    {:ok, _, relative_expiration} = DateInterpreter.interpret_expiration("in fifteen hours", test_start_date)
+    assert relative_expiration == "in 15 hours, on Wednesday, April 5, 2017 at 1:25AM"
+  end
+
+  test "future expiration shifted by hours and minutes that spills into the next day" do
+    test_start_date = Timex.to_datetime({{2017, 4, 4}, {10, 25, 0}})
+    {:ok, _, relative_expiration} = DateInterpreter.interpret_expiration("in fifteen hours and thirty six minutes", test_start_date)
+    assert relative_expiration == "in 15 hours, on Wednesday, April 5, 2017 at 2:01AM"
+  end
+
+end
--- web/modules/date_interpreter.ex
+++ web/modules/date_interpreter.ex
@@ -3,36 +3,48 @@ defmodule Butler.DateInterpreter do
   alias Butler.Expiration
 
   # Parse expiration from format "in X weeks" or a specified date
-  @spec interpret_expiration(String.t, Datetime.t) :: {:ok, Datetime.t, String.t} | {:error, String.t}
+  @spec interpret_expiration(String.t, Datetime.t) :: {:ok, Datetime.t, String.t} | {:error, Datetime.t}
   def interpret_expiration(user_expiration, start_date \\ Timex.now) do
-    case String.split(user_expiration).first do
+    case String.split(user_expiration) |> List.first do
       nil ->
         IO.puts "interpret_expiration: empty string passed in!"
       "in" ->
         expiration = Expiration.from_relative_string(user_expiration)
-        expiration_date = Timex.shift(start_date, seconds: expiration.seconds,
-             minutes: expiration.minutes, hours: expiration.hours, days: expiration.days,
-             months: expiration.months, years: expiration.years)
-
-        # Ex. 20 days from now, on Monday, January 23, 2017
-        case Timex.format(expiration_date, "{relative}", Timex.Format.DateTime.Formatters.Relative) do
-          {:error, term} ->
-            IO.puts item <> " RELATIVE could not be found in list"
-            {:error, item}
-          {:ok, relative_component} ->
-            case Timex.format(expiration_date, "{WDfull}, {Mfull} {D}, {YYYY}") do
-              {:error, term} ->
-                IO.puts item <> " could not be found in list"
-                {:error, item}
-              {:ok, full_component} ->
-                expiration_string = Enum.join([relative_component, full_component], ", on ")
-                {:ok, expiration_date, expiration_string}
-            end
-        end
+        relative_response_from_expiration(expiration, start_date)
       "on" ->
         Expiration.from_exact_string(user_expiration)
       _ ->
         IO.puts "interpret_expiration: no matching pattern!"
+    end
+  end
+
+  defp relative_response_from_expiration(expiration, start_date) do
+    expiration_date = Timex.shift(start_date, seconds: expiration.seconds,
+         minutes: expiration.minutes, hours: expiration.hours, days: expiration.days,
+         months: expiration.months, years: expiration.years)
+    # Ex. in 20 days, on Monday, January 23, 2017
+    case Timex.Format.DateTime.Formatters.Relative.relative_to(expiration_date, start_date, "{relative}") do
+      {:error, failed_date} ->
+        IO.puts "Failed to use relative format for expiration_date: " <> failed_date
+        {:error, failed_date}
+      {:ok, relative_component} ->
+        exact_component_format = exact_component_format_from_expiration(expiration)
+        case Timex.format(expiration_date, exact_component_format) do
+          {:error, failed_date} ->
+            IO.puts "Failed to convert to full date format for expiration date: " <> failed_date
+            {:error, expiration_date}
+          {:ok, exact_component} ->
+            expiration_string = Enum.join([relative_component, exact_component], ", on ")
+            {:ok, expiration_date, expiration_string}
+        end
+    end
+  end
+
+  defp exact_component_format_from_expiration(expiration) do
+    if Expiration.includes_time_components(expiration) do
+      "{WDfull}, {Mfull} {D}, {YYYY} at {h12}:{m}{AM}"
+    else
+      "{WDfull}, {Mfull} {D}, {YYYY}"
     end
   end
 end
